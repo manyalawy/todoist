9454e1280792589edfffa997436f296b240dce5f
manyalawy <manyalawy@gmail.com>
2022-05-14 17:05:35 +0200

    rabbitmq

---
+++ list/src/main/java/com/todoist/list/commands/Command.java
@@ -0,0 +1,5 @@
+package com.todoist.list.commands;
+
+public interface Command {
+    public void execute();
+}
---
+++ list/src/main/java/com/todoist/list/commands/CreateTodoList.java
@@ -0,0 +1,13 @@
+package com.todoist.list.commands;
+
+public class CreateTodoList implements Command {
+
+
+    @Override
+    public void execute() {
+//        Read from message queue
+//        parse message
+//        implement logic
+//        publish on message queue
+    }
+}
