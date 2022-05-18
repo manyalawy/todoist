8ae574ea2b178b572ec5b82d7ceef9cc0f7de6e2
manyalawy <manyalawy@gmail.com>
2022-05-18 19:13:32 +0200

    resturucture

--- list/src/main/java/com/todoist/list/repo/TaskRepo.java
+++
@@ -1,6 +0,0 @@
-//package com.todoist.list.repo;
-//
-//import com.todoist.list.model.Task;
-//import org.springframework.data.mongodb.repository.MongoRepository;
-//
-//public interface TaskRepo extends MongoRepository<Task, String> {}
